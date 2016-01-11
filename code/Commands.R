98d5315bc8b278551b1dc771abd4cc9b4d4424b3
hywel <hywel.stoakes@gmail.com>
2016-01-11 13:47:08 +1100

    cofig update

--- code/Commands.R
+++ code/Commands.R
@@ -1,7 +1,10 @@
 servr::jekyll(command="bundle exec jekyll build")
 servr::jekyll(command="bundle exec jekyll build", serve = FALSE)
 
+# bundle exec octopress new draft "My Title" --dir blog
 # bundle exec octopress new post "New Post Title" --dir posts
 # bundle exec octopress new page new-page/
 
 #This will create a page at /new-page/index.md
+
+
