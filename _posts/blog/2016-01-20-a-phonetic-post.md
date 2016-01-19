a6377b246690d0a0edaff865f2090991d3e58c4c
hywel <hywel.stoakes@gmail.com>
2016-01-20 00:16:34 +1100

    Phonetic Post

---
+++ _posts/blog/2016-01-20-a-phonetic-post.md
@@ -0,0 +1,22 @@
+---
+layout: post
+title: A Phonetic Post
+status: publish
+published: true
+modified:
+categories: blog
+excerpt:
+tags: []
+image:
+  feature:
+date: 2016-01-20T00:06:55+11:00
+---
+ 
+Here we have a phonetic post. Time to test the phonetic fonts and unicode compliance.
+ 
+* θɪs ɪs eɪ pɐʉst wɪθ ə fɔˈnɛtɪk ˈkɔmpɐʉnənt
+* This is a post with a fonetic componant
+ 
+![Spectrogram](/figures/spectrogram1.png)
+ 
+ 
--- code/Sequence.R
+++ code/Sequence.R
@@ -1,9 +1,20 @@
 source("code/rmd2md.R")
 rmd2md = rmd2md
 # This creates a new rmd post
-system('bundle exec octopress new post "New Blog Post Title" --dir blog')
+# blog.title <- "New Blog Post Title"
+blog.title <- "A Phonetic Post"
+blog.path <-system(paste("bundle exec octopress new post ", blog.title ," --dir blog", sep = ""),intern=TRUE)
+#blog.path <- as.list(blog.path)
+# Edit the post
+
+# process the rmd to md file
 rmd2md(dir_rmd = "_posts/blog", dir_md = "_posts/blog") # for a blog post
+
+# article.title <- "New Article Title"
+# system(paste("bundle exec octopress new post ", article.title ," --dir article", sep = ""))
 #rmd2md(dir_rmd = "_posts/articles", dir_md = "_posts/articles") # for an article
+
+#
 servr::jekyll(command="bundle exec jekyll build")
 servr::jekyll(command="bundle exec jekyll build", serve = FALSE)
 
