a3d9ee64c2c50aef903a4d735cbbe090604e6e5b
hywel <hywel.stoakes@gmail.com>
2016-01-19 23:01:04 +1100

    comments redux

---
+++ code/Sequence.R
@@ -0,0 +1,7 @@
+source("code/rmd2md.R")
+rmd2md = rmd2md
+# This creates a new rmd post
+system('bundle exec octopress new post "New Blog Post Title" --dir blog')
+rmd2md(dir_rmd = "_posts/blog", dir_md = "_posts/blog") # for a blog post
+#rmd2md(dir_rmd = "_posts/articles", dir_md = "_posts/articles") # for an article
+servr::jekyll(command="bundle exec jekyll build")
