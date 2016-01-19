ee07e4a7cda4099d4256e7fe1669af1ee8894e4e
hywel <hywel.stoakes@gmail.com>
2016-01-19 23:03:50 +1100

    s

--- _site/code/Sequence.R
+++ _site/code/Sequence.R
@@ -5,3 +5,5 @@ system('bundle exec octopress new post "New Blog Post Title" --dir blog')
 rmd2md(dir_rmd = "_posts/blog", dir_md = "_posts/blog") # for a blog post
 #rmd2md(dir_rmd = "_posts/articles", dir_md = "_posts/articles") # for an article
 servr::jekyll(command="bundle exec jekyll build")
+servr::jekyll(command="bundle exec jekyll build", serve = FALSE)
+
