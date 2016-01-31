628a9c0af6321e917f32f822358f13612850b91e
hywel <hywel.stoakes@gmail.com>
2016-01-31 14:01:55 +1100

    ignore

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source "https://rubygems.org"
 
 gem 'jekyll', '2.5.3'
+gem 'jekyll-gist'
 gem 'jekyll-sitemap'
 gem 'rouge'
 gem 'octopress', '~> 3.0'
--- code/Sequence.R
+++ code/Sequence.R
@@ -1,22 +1,40 @@
+require(servr)
 source("code/rmd2md.R")
 rmd2md = rmd2md
+
+#### Create a Blog Post ####
 # This creates a new rmd post
 # blog.title <- "New Blog Post Title"
-blog.title <- "A Few Preliminaries"
+blog.title <- ""
 blog.path <-system(paste("bundle exec octopress new post ", blog.title ," --dir blog", sep = ""),intern=TRUE)
 #blog.path <- as.list(blog.path)
 # Edit the post
 
+
+#-----------------------------#
 # process the rmd to md file
 rmd2md(dir_rmd = "_posts/blog", dir_md = "_posts/blog") # for a blog post
 
+#### Create a Draft ####
+draft.title <- "draft1"
+system(paste("bundle exec octopress new draft ", draft.title , sep = ""))
+#-----------------------------#
+
+#### Create an Article ####
 # article.title <- "New Article Title"
 # system(paste("bundle exec octopress new post ", article.title ," --dir article", sep = ""))
 #rmd2md(dir_rmd = "_posts/articles", dir_md = "_posts/articles") # for an article
 
-#
+#-----------------------------#
+
+#### Convert a Draft into a Post ####
+blog.path <-system(paste("bundle exec octopress publish"))
+#-----------------------------#
+
+#### Build the site using Jekyll ####
 #servr::jekyll(command="bundle exec jekyll build") # Use this if you want to preview the site
 servr::jekyll(command="bundle exec jekyll build", serve = FALSE)
 
 # Then use git to commit the changes and then push to github
 
+#-----------------------------#
