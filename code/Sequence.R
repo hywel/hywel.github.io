645ca52265eef5fb52abaf70e2d74df448b5eda8
hywel <hywel.stoakes@gmail.com>
2016-08-10 20:37:42 +1000

    hmmmm

--- code/Sequence.R
+++ code/Sequence.R
@@ -1,7 +1,8 @@
 require(servr, quietly=TRUE)
 source("code/rmd2md.R")
+source("code/knitpages.R")
 rmd2md = rmd2md
-
+knit_folder = knit_folder
 #### Create a Blog Post ####
 # This creates a new rmd post
 # blog.title <- "New Blog Post Title"
@@ -14,7 +15,8 @@ blog.path <-system(paste("bundle exec octopress new post ", blog.title ," --dir 
 file.edit(blog.path)
 warning("Edit the blog file and then return to Sequence.R to process and push to git")
 # process the rmd to md file
-rmd2md(dir_rmd = "_posts/blog", dir_md = "_posts/blog") # for a blog post
+# rmd2md(dir_rmd = "_posts/blog", dir_md = "_posts/blog") # for a blog post
+knit_folder("_R", "_posts/blog", "figs/", "_caches/") # for a blog post
 servr::jekyll(command="bundle exec jekyll build", serve = FALSE)
 
 ##-----------------------------##
@@ -27,7 +29,8 @@ system(paste("bundle exec octopress new draft ", draft.title , sep = ""))
 #### Create an Article ####
 article.title <- readline("What would you like to name your post?")
 system(paste("bundle exec octopress new post ", article.title ," --dir article", sep = ""),intern=TRUE)
-rmd2md(dir_rmd = "_posts/articles", dir_md = "_posts/articles") # for an article
+# rmd2md(dir_rmd = "_posts/articles", dir_md = "_posts/articles") # for an article
+knit_folder("_R", "_posts/articles", "figs/", "_caches/")# for an article
 
 #-----------------------------#
 
